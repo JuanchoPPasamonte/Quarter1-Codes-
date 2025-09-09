5d917def71b2d2fb701477aca2d287ea459ba5b9
JuanchoPPasamonte <juancho.pasamonte@smc.pshs.edu.ph>
2025-09-09 10:35:59 +0800

    Code1.py
    Add 3 numbers

---
+++ Code1.py
@@ -0,0 +1,5 @@
+Num1 = float(input("What is your first number"))
+Num2 = float(input("What is your socond number"))
+Num3 = float(input("What is your third number"))
+Sum = Num1 + Num2 + Num3
+print("The result is",Sum)
