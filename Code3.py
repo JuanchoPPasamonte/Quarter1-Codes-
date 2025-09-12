707b4cb75da60ad256632b10097f4080f17a1386
JuanchoPPasamonte <juancho.pasamonte@smc.pshs.edu.ph>
2025-09-12 11:55:40 +0800

    Code 3 of the quarter
    Distance formula

---
+++ Code3.py
@@ -0,0 +1,6 @@
+X1 = float(input("First Number or X1:"))
+X2 = float(input("Second Number or X2:"))
+Y1 = float(input("Third Number or Y1:"))
+Y2 = float(input("Fourth Number:Y2"))
+X1 = (X1-X2)**2+(Y1-Y2)**2
+print("Distance is:",X1)
